0093b2c8f70b7711572e3538dfb36910849c24d4
ianrahman <ian.alexander.rahman@gmail.com>
2016-09-08 14:20:36 -0400

    Converted data store to struct

--- SlapChat/DataStore.swift
+++ SlapChat/DataStore.swift
@@ -9,7 +9,7 @@
 import Foundation
 import CoreData
 
-class DataStore {
+struct DataStore {
     
 
     static let sharedDataStore =  DataStore()
@@ -17,7 +17,7 @@ class DataStore {
     
     // MARK: - Core Data Saving support
     
-    func saveContext () {
+    mutating func saveContext () {
         if managedObjectContext.hasChanges {
             do {
                 try managedObjectContext.save()
@@ -31,7 +31,7 @@ class DataStore {
         }
     }
     
-//        func fetchData ()
+//        mutating func fetchData ()
 //        {
 //         perform a fetch request to fill an array property on your datastore
 //        }
